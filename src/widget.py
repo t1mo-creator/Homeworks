50c809735883accdb197033e30a81edf68f780db
Denis <reebook_2018@mail.ru>
2025-02-26 21:23:26 +1000

    HOMEWORK 10.2 fix

--- src/widget.py
+++ src/widget.py
@@ -15,19 +15,19 @@ def mask_account_card(card_info: str) -> str:
             returned_number = masks.get_mask_card_number(card_infolist.pop(-1))
         else:
             raise ValueError("номер карты не указан")
-    card_infolist.append(returned_number)
-    masked_number = " ".join(card_infolist)
-    return str(masked_number)
+        card_infolist.append(returned_number)
+        masked_number = " ".join(card_infolist)
+        return str(masked_number)
 
 
 def get_date(core_date: str) -> str:
     """Принимает дату и время в формате ISO 8601, возвращает дату в формате ДД.ММ.ГГГГ"""
     core_date_list = core_date.split("-")
     if (
-        core_date_list[0].isdigit()
-        and core_date_list[1].isdigit()
-        and core_date_list[2][:2].isdigit
-        and len(core_date_list) == 3
+            core_date_list[0].isdigit()
+            and core_date_list[1].isdigit()
+            and core_date_list[2][:2].isdigit
+            and len(core_date_list) == 3
     ):
         returned_date = core_date_list[2][:2] + "." + core_date_list[1] + "." + core_date_list[0]
         return returned_date
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,19 +3,41 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
+@pytest.mark.parametrize(
+    "value",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Mastercard 1234567890123456", "Mastercard 1234 56** **** 3456"),
+        ("Мир 2200123456789010", "Мир 2200 12** **** 9010"),
+        ("Union Pay 6010203040506070", "Union Pay 6010 20** **** 6070"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("73654108430135874305", "**4305"),
+    ],
+)
 def test_mask_account_card(value):
     assert mask_account_card(value)
 
 
+@pytest.mark.parametrize(
+    "value",
+    [
+        ("Visa Platinum ", "номер карты не указан"),
+        ("Счет ", "номер счета не указан"),
+        ("", "нет данных"),
+        ("1", "некорректный номер карты"),
+    ],
+)
 def test_mask_account_card_valueerror(value):
     with pytest.raises(ValueError):
         mask_account_card(value)
 
 
-def test_get_date(value):
-    assert get_date(value)
-
-
-def test_get_date_valueerror(value):
+def test_mask_account_card_valueerror():
     with pytest.raises(ValueError):
-        get_date(value)
+        mask_account_card("")
+
+
+def test_get_date_valueerror():
+    with pytest.raises(ValueError):
+        get_date("")
